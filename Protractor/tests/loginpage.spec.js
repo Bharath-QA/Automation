aea2e1f91ad13a25c9995d4adf6791cc6cad6703
Bharath-QA <bharath.techui@gmail.com>
2021-03-07 14:10:26 -0600

    add jasmine blocks- beforeEach, afterEach, beforeAll, afterAll methods

--- Protractor/tests/loginpage.spec.js
+++ Protractor/tests/loginpage.spec.js
@@ -1,6 +1,41 @@
-let loginPageElement = require('../pages/loginPage');
+// let loginPageElement = require('../pages/loginPage');
 
-describe('A login page',function(){
+describe('A login page', function () {
+
+    /*
+    //This will execute multiple time of 'it' before each 'it' block
+    beforeEach(function () {
+        console.log("test 'beforEach' block");
+    });
+    
+    //This will execute multiple time of 'it' after each 'it' block
+    afterEach(function(){
+        console.log("test 'afterEach' block");
+    });
+
+    //This will execute only once before 'it' blocks
+    beforeAll(function(){
+        console.log("test 'beforeAll' block");
+    });
+
+    //This will execute only once after 'it' blocks
+    afterAll(function(){
+        console.log("test 'afterAll' block");
+    });
+
+    it('test it block', function(){
+        console.log('test it block 1');
+    });
+
+    it('test it block', function(){
+        console.log('test it block 2');
+    });
+
+    it('test it block', function(){
+        console.log('test it block 3');
+    });
+
+    */
 
     it('login page', function(){
 
@@ -10,12 +45,12 @@ describe('A login page',function(){
 
         loginPageElement.loginInput('test@gmail.com');
         loginPageElement.pwdInput('test@1213');
-        
+
         browser.sleep(3000);
         loginPageElement.submitButton();
-        
+
         browser.sleep(10000);
-        
-    })
-    
+
+    });
+
 })
